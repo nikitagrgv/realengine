be722ab4fce52f9c04f6db439cb6ed8c0a7b2737
Nikita Grigorev <pettyaelse@gmail.com>
2024-08-17 20:35:15 +0700

    hello triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,32 @@
 int main()
 {
     glfwInit();
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(0.0f, 0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
