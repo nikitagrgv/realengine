73974747d174fd298435cb6ad1312c8f707fbf28
Nikita Grigorev <pettyaelse@gmail.com>
2024-08-18 15:48:20 +0700

    w

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -135,18 +135,18 @@ public:
 
         float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
 
-        unsigned int VBO;
-        glGenBuffers(1, &VBO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-
-        unsigned int VAO;
+        unsigned int VBO, VAO;
         glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
         glBindVertexArray(VAO);
+
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
         glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
 
         Shader shader("shader.shader");
 
@@ -169,6 +169,8 @@ public:
                 exit_ = true;
             }
         }
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
     }
 
 private:
