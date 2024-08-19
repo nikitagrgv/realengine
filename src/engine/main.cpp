86bf3108af8ea321fc5953c61b2ceeeab64bb2ab
Nikita Grigorev <pettyaelse@gmail.com>
2024-08-19 23:59:44 +0700

    w

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -338,22 +338,22 @@ public:
         cat_mesh.addAttributeFloat(2);
         cat_mesh.addVertex({
             {0.5f, 0.5f, 0.0f},
-            {},
+            {0, 0, 1},
             {1.0f, 1.0f}
         });
         cat_mesh.addVertex({
             {0.5f, -0.5f, 0.0f},
-            {},
+            {0, 0, 1},
             {1.0f, 0.0f}
         });
         cat_mesh.addVertex({
             {-0.5f, -0.5f, 0.0f},
-            {},
+            {0, 0, 1},
             {0.0f, 0.0f}
         });
         cat_mesh.addVertex({
             {-0.5f, 0.5f, 0.0f},
-            {},
+            {0, 0, 1},
             {0.0f, 1.0f}
         });
         cat_mesh.addIndices({0, 1, 3, 1, 2, 3});
