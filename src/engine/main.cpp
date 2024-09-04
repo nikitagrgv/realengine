8b8e5d0f37ee51b3be63a7dfd91c1b0af119e335
Nikita Grigorev <pettyaelse@gmail.com>
2024-09-04 15:52:56 +0700

    w ui

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -209,61 +209,65 @@ public:
                 ImGui::SetNextWindowSize(ImVec2(380, 340), ImGuiCond_FirstUseEver);
                 ImGui::SetNextWindowPos(ImVec2(0, 200), ImGuiCond_FirstUseEver);
 
-                if (ImGui::Begin("Example: Simple layout", &materials_window_,
+                if (!ImGui::Begin("Example: Simple layout", &materials_window_,
                         ImGuiWindowFlags_MenuBar))
                 {
-                    // Left
-                    static int selected = 0;
-                    {
-                        ImGui::BeginChild("left pane", ImVec2(150, 0),
-                            ImGuiChildFlags_Border | ImGuiChildFlags_ResizeX);
-                        for (int i = 0; i < 100; i++)
-                        {
-                            // FIXME: Good candidate to use ImGuiSelectableFlags_SelectOnNav
-                            char label[128];
-                            sprintf(label, "MyObject %d", i);
-                            if (ImGui::Selectable(label, selected == i))
-                            {
-                                selected = i;
-                            }
-                        }
-                        ImGui::EndChild();
-                    }
-                    ImGui::SameLine();
-
-                    // Right
-                    {
-                        ImGui::BeginGroup();
-                        ImGui::BeginChild("item view",
-                            ImVec2(0, -ImGui::GetFrameHeightWithSpacing())); // Leave room for 1
-                                                                             // line below us
-                        ImGui::Text("MyObject: %d", selected);
-                        ImGui::Separator();
-                        if (ImGui::BeginTabBar("##Tabs", ImGuiTabBarFlags_None))
-                        {
-                            if (ImGui::BeginTabItem("Description"))
-                            {
-                                ImGui::TextWrapped("Lorem ipsum dolor sit amet, consectetur "
-                                                   "adipiscing elit, sed do eiusmod tempor "
-                                                   "incididunt ut labore et dolore magna aliqua. ");
-                                ImGui::EndTabItem();
-                            }
-                            if (ImGui::BeginTabItem("Details"))
-                            {
-                                ImGui::Text("ID: 0123456789");
-                                ImGui::EndTabItem();
-                            }
-                            ImGui::EndTabBar();
-                        }
-                        ImGui::EndChild();
-                        if (ImGui::Button("Revert"))
-                        {}
-                        ImGui::SameLine();
-                        if (ImGui::Button("Save"))
-                        {}
-                        ImGui::EndGroup();
-                    }
+                    ImGui::End();
+                    return;
                 }
+
+                // Left
+                static int selected = 0;
+                {
+                    ImGui::BeginChild("left pane", ImVec2(150, 0),
+                        ImGuiChildFlags_Border | ImGuiChildFlags_ResizeX);
+                    for (int i = 0; i < 100; i++)
+                    {
+                        // FIXME: Good candidate to use ImGuiSelectableFlags_SelectOnNav
+                        char label[128];
+                        sprintf(label, "MyObject %d", i);
+                        if (ImGui::Selectable(label, selected == i))
+                        {
+                            selected = i;
+                        }
+                    }
+                    ImGui::EndChild();
+                }
+                ImGui::SameLine();
+
+                // Right
+                {
+                    ImGui::BeginGroup();
+                    ImGui::BeginChild("item view",
+                        ImVec2(0, -ImGui::GetFrameHeightWithSpacing())); // Leave room for 1
+                                                                         // line below us
+                    ImGui::Text("MyObject: %d", selected);
+                    ImGui::Separator();
+                    if (ImGui::BeginTabBar("##Tabs", ImGuiTabBarFlags_None))
+                    {
+                        if (ImGui::BeginTabItem("Description"))
+                        {
+                            ImGui::TextWrapped("Lorem ipsum dolor sit amet, consectetur "
+                                               "adipiscing elit, sed do eiusmod tempor "
+                                               "incididunt ut labore et dolore magna aliqua. ");
+                            ImGui::EndTabItem();
+                        }
+                        if (ImGui::BeginTabItem("Details"))
+                        {
+                            ImGui::Text("ID: 0123456789");
+                            ImGui::EndTabItem();
+                        }
+                        ImGui::EndTabBar();
+                    }
+                    ImGui::EndChild();
+                    if (ImGui::Button("Revert"))
+                    {}
+                    ImGui::SameLine();
+                    if (ImGui::Button("Save"))
+                    {}
+                    ImGui::EndGroup();
+                }
+
                 ImGui::End();
             }
 
