13d0c74db3d752fcc8e652c70fa55a63034686dd
Nikita Grigorev <pettyaelse@gmail.com>
2024-09-05 15:58:51 +0700

    w ui

--- src/engine/Node.cpp
+++ src/engine/Node.cpp
@@ -1,1 +1,3 @@
 #include "Node.h"
+
+Node::~Node() {}
--- src/engine/Node.h
+++ src/engine/Node.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <glm/mat4x4.hpp>
+#include <string>
 
-class Node {
+class Node
+{
+public:
+    virtual ~Node();
 
+    const std::string &getName() const { return name_; }
+    void setName(std::string name) { name_ = std::move(name); }
+
+    const glm::mat4 &getTransform() const { return transform_; }
+    void setTransform(const glm::mat4 &transform) { transform_ = transform; }
+
+private:
+    std::string name_;
+    glm::mat4 transform_{1.0f};
 };
-
-
