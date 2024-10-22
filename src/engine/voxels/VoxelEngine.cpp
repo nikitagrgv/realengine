7084b782a9c549b62d1d073b51cf82f1e6c3cc81
Nikita Grigorev <pettyaelse@gmail.com>
2024-10-22 12:00:54 +0700

    w vox

--- src/engine/voxels/VoxelEngine.cpp
+++ src/engine/voxels/VoxelEngine.cpp
@@ -1,6 +1,8 @@
 #include "VoxelEngine.h"
 
 #include "BlocksRegistry.h"
+#include "EngineGlobals.h"
+#include "TextureManager.h"
 
 VoxelEngine::VoxelEngine() = default;
 
@@ -9,4 +11,8 @@ VoxelEngine::~VoxelEngine() = default;
 void VoxelEngine::init()
 {
     registry_ = makeU<BlocksRegistry>();
+
+    Texture *atlas = eng.texture_manager->create("atlas");
+    atlas->load("vox/atlas.png", Texture::Format::RGBA, Texture::Wrap::ClampToEdge,
+        Texture::Filter::Nearest, Texture::Filter::Nearest, false);
 }
