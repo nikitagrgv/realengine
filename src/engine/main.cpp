8d749a7b2b39e9ff00fad1e2e944d303cb2eb4d7
Nikita Grigorev <pettyaelse@gmail.com>
2024-10-22 16:40:40 +0700

    w vox

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -463,7 +463,7 @@ private:
         int height = window->getHeight();
         width = std::max(1, width);
         height = std::max(1, height);
-        camera_.setPerspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
+        camera_.setPerspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
     }
 
 private:
