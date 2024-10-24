890ef57ab0bc940be33ac824f920b6899204930b
Nikita Grigorev <pettyaelse@gmail.com>
2024-10-25 00:31:53 +0700

    w

--- src/engine/profiler/ScopedProfiler.cpp
+++ src/engine/profiler/ScopedProfiler.cpp
@@ -1,1 +1,54 @@
 #include "ScopedProfiler.h"
+
+#include <immintrin.h>
+
+#include <cassert>
+#include <cstdint>
+
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
+
+namespace
+{
+
+#ifdef _WIN32
+LARGE_INTEGER PERF_FREQ{};
+#endif
+
+} // namespace
+
+namespace
+{
+
+// TODO# don't divide here. do it in statistic collection stage
+uint64_t get_time_usec()
+{
+#ifdef _WIN32
+    LARGE_INTEGER tval;
+    QueryPerformanceCounter(&tval);
+
+    unsigned long long counter_high_part = 0;
+    unsigned long long counter = tval.QuadPart;
+
+    counter = _umul128(counter, 1'000'000, &counter_high_part);
+    #if _MSC_VER >= 1920
+    return _udiv128(counter_high_part, counter, PERF_FREQ.QuadPart, nullptr);
+    #endif
+#else
+    // TODO#
+    return 0;
+#endif
+}
+
+} // namespace
+
+
+ScopedProfiler::ScopedProfiler(const char *name)
+    : start_(get_time_usec())
+{}
+
+ScopedProfiler::~ScopedProfiler()
+{
+    const uint64_t end = get_time_usec();
+}
--- src/engine/profiler/ScopedProfiler.h
+++ src/engine/profiler/ScopedProfiler.h
@@ -1,5 +1,13 @@
 #pragma once
 
-class ScopedProfiler {
+#include <cstdint>
 
+class ScopedProfiler
+{
+public:
+    explicit ScopedProfiler(const char *name);
+    ~ScopedProfiler();
+
+private:
+    uint64_t start_;
 };
