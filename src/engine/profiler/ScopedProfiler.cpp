6906d167aaa32727564da01600e91c3e46330108
Nikita Grigorev <pettyaelse@gmail.com>
2024-10-25 23:54:46 +0700

    wip profiler

--- src/engine/profiler/ScopedProfiler.cpp
+++ src/engine/profiler/ScopedProfiler.cpp
@@ -242,7 +242,7 @@ void dump_svg()
     };
 
     sprintf(TEMP_BUFFER,
-        R"!(<svg width="100%%" height="100%%" viewBox="0 0 %lld %lld" xmlns="http://www.w3.org/2000/svg">)!",
+        R"!(<svg width="100%%" height="100%%" viewBox="0 0 %lf %lf" xmlns="http://www.w3.org/2000/svg">)!",
         total_width, total_height);
     out << TEMP_BUFFER << "\n";
 
