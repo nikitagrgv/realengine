4d4bc7a8fa02eaab0fdd5cc2815cca53b66e6f72
Nikita Grigorev <pettyaelse@gmail.com>
2024-10-28 00:19:35 +0700

    wip thread

--- src/engine/threads/Thread.cpp
+++ src/engine/threads/Thread.cpp
@@ -1,1 +1,44 @@
 #include "Thread.h"
+
+#include <windows.h>
+
+namespace
+{
+
+DWORD WINAPI run_thread(LPVOID param)
+{
+    auto thread = static_cast<Thread *>(param);
+    thread->execute();
+    return 0;
+}
+
+} // namespace
+
+Thread::Thread()
+{
+    DWORD thread_id;
+    handle_ = CreateThread(nullptr, // Default security attributes
+        0,                          // Default stack size
+        &run_thread,                // Thread function
+        this,                       // Thread parameters
+        0,                          // Default creation flags
+        &thread_id                  // Receive thread ID
+    );
+    id_ = thread_id;
+}
+
+Thread::~Thread()
+{
+    join();
+}
+
+void Thread::join()
+{
+    if (handle_)
+    {
+        WaitForSingleObject(handle_, INFINITE);
+        CloseHandle(handle_);
+        handle_ = nullptr;
+        id_ = 0;
+    }
+}
--- src/engine/threads/Thread.h
+++ src/engine/threads/Thread.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include "Base.h"
 
+#include <cstdint>
 
-class Thread {
+class Thread
+{
+public:
+    REMOVE_COPY_MOVE_CLASS(Thread);
 
+    Thread();
+    ~Thread();
+
+    void join();
+
+    virtual void execute() = 0;
+
+private:
+    void *handle_{};
+    uint64_t id_{};
 };
