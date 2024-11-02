721e70edd56a27d9dc767dc4ce10ff54dac1ac43
Nikita Grigorev <pettyaelse@gmail.com>
2024-11-03 02:45:37 +0700

    w

--- src/engine/threads/WorkerThread.cpp
+++ src/engine/threads/WorkerThread.cpp
@@ -11,6 +11,7 @@ namespace tbb
 
 void WorkerThread::execute()
 {
+    set_state(State::Idle);
     while (true)
     {
         if (needExit())
@@ -25,12 +26,28 @@ void WorkerThread::execute()
         }
 
         ScopedProfiler prof("WorkerThread::Execute job");
-
+        set_state(State::Working);
         job->execute();
         job->finishWorkerThread();
 
         eng.queue->addFinishedJob(std::move(job));
+        set_state(State::Idle);
     }
+}
+
+WorkerThread::State WorkerThread::getState() const
+{
+    const int state_value = state_.load();
+    const State state = static_cast<State>(state_value);
+    assert(state == State::Idle || state == State::Working);
+    return state;
+}
+
+void WorkerThread::set_state(State state)
+{
+    assert(state == State::Idle || state == State::Working);
+    const int state_value = static_cast<int>(state);
+    state_.store(state_value);
 }
 
 } // namespace tbb
--- src/engine/threads/WorkerThread.h
+++ src/engine/threads/WorkerThread.h
@@ -8,7 +8,22 @@ namespace tbb
 class WorkerThread : public Thread
 {
 public:
+    enum class State
+    {
+        Idle,
+        Working,
+    };
+
+public:
     void execute() override;
+
+    State getState() const;
+
+private:
+    void set_state(State state);
+
+private:
+    std::atomic<int> state_;
 };
 
 } // namespace tbb
