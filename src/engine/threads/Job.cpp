74b7611e52b6d1bf2ae8a596e8109a8dc6c046c1
Nikita Grigorev <pettyaelse@gmail.com>
2024-11-03 02:59:37 +0700

    w

--- src/engine/threads/Job.cpp
+++ src/engine/threads/Job.cpp
@@ -1,1 +1,54 @@
 #include "Job.h"
+
+#include <atomic>
+
+tbb::CancelToken::CancelToken(std::shared_ptr<std::atomic<bool>> job_alive,
+    std::shared_ptr<std::atomic<bool>> cancel_flag)
+    : job_alive_(std::move(job_alive))
+    , cancel_flag_(std::move(cancel_flag))
+{
+    assert(job_alive_);
+    assert(cancel_flag_);
+}
+
+bool tbb::CancelToken::isCanceled() const
+{
+    return cancel_flag_->load();
+}
+
+void tbb::CancelToken::cancel()
+{
+    cancel_flag_->store(true);
+}
+
+bool tbb::CancelToken::isAlive() const
+{
+    return job_alive_->load();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+tbb::Job::Job()
+    : job_alive_(std::make_shared<std::atomic<bool>>(true))
+    , cancel_flag_(std::make_shared<std::atomic<bool>>(false))
+{}
+
+tbb::Job::~Job()
+{
+    job_alive_->store(false);
+}
+
+bool tbb::Job::isCanceled() const
+{
+    return cancel_flag_->load();
+}
+
+void tbb::Job::cancel()
+{
+    cancel_flag_->store(true);
+}
+
+tbb::CancelToken tbb::Job::getCancelToken() const
+{
+    return CancelToken(job_alive_, cancel_flag_);
+}
--- src/engine/threads/Job.h
+++ src/engine/threads/Job.h
@@ -4,18 +4,42 @@
 namespace tbb
 {
 
+class CancelToken
+{
+public:
+    explicit CancelToken(std::shared_ptr<std::atomic<bool>> job_alive,
+        std::shared_ptr<std::atomic<bool>> cancel_flag);
+
+    bool isCanceled() const;
+    void cancel();
+
+    bool isAlive() const;
+
+private:
+    std::shared_ptr<std::atomic<bool>> job_alive_;
+    std::shared_ptr<std::atomic<bool>> cancel_flag_;
+};
+
 class Job
 {
 public:
     REMOVE_COPY_MOVE_CLASS(Job);
 
-    Job() = default;
-    virtual ~Job() = default;
+    Job();
+    virtual ~Job();
 
     virtual void execute() = 0;
 
+    bool isCanceled() const;
+    void cancel();
+    CancelToken getCancelToken() const;
+
     virtual void finishWorkerThread() {}
     virtual void finishMainThread() {}
+
+private:
+    std::shared_ptr<std::atomic<bool>> job_alive_;
+    std::shared_ptr<std::atomic<bool>> cancel_flag_;
 };
 
 } // namespace tbb
