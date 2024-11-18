4370346d446919ea800daadf1db6c7173eb3e0b3
Nikita Grigorev <pettyaelse@gmail.com>
2024-11-18 21:29:21 +0700

    w intersection

--- src/engine/voxels/VoxelEngine.cpp
+++ src/engine/voxels/VoxelEngine.cpp
@@ -639,9 +639,9 @@ VoxelEngine::IntersectionResult VoxelEngine::getIntersection(const glm::vec3 &po
         position + glm::vec3{0.004, 0.002, 0.002} + glm::vec3{0, 0, 1} * dir_n.z * t_max_z,
         glm::vec4{0, 0, 1, 0.7}, true, 95);
 
-    const float dx = t_max_x;
-    const float dy = t_max_y;
-    const float dz = t_max_z;
+    const float dx = std::abs(1.0f / dir_n.x);
+    const float dy = std::abs(1.0f / dir_n.y);
+    const float dz = std::abs(1.0f / dir_n.z);
 
     do
     {
